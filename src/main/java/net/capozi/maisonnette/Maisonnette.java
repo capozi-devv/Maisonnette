2fee57baa59dca94324d9ceadb4ef67b340e9e31
Cosmo <88160302+CosmoLeveling@users.noreply.github.com>
2025-07-14 23:10:55 -0500

    Update Maisonnette.java

--- src/main/java/net/capozi/maisonnette/Maisonnette.java
+++ src/main/java/net/capozi/maisonnette/Maisonnette.java
@@ -10,9 +10,11 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// NOTE: main
 public class Maisonnette implements ModInitializer {
 	public static final String MOD_ID = "maisonnette";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
 	@Override
 	public void onInitialize() {
 		BlockInit.init();
