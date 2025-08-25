9c8cd410e3b08b539078b282d4dbfc8a077175e0
Capozi <rslewis009@gmail.com>
2025-08-25 18:42:32 -0400

    Block state gen and South models in progress

--- src/main/java/net/capozi/maisonnette/common/block/MosaicBlock.java
+++ src/main/java/net/capozi/maisonnette/common/block/MosaicBlock.java
@@ -3,21 +3,39 @@ package net.capozi.maisonnette.common.block;
 import net.capozi.maisonnette.foundation.BlockInit;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.loot.context.LootContextParameterSet;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.IntProperty;
+import net.minecraft.state.property.Properties;
 
 import java.util.Collections;
 import java.util.List;
 
 public class MosaicBlock extends Block {
     public static final IntProperty TILES = IntProperty.of("tiles", 1, 4);
+    public static final BooleanProperty WATERLOGGED;
+    public static final DirectionProperty FACING;
     public MosaicBlock(Settings settings) {
         super(settings);
     }
     @Override
     public List<ItemStack> getDroppedStacks(BlockState state, LootContextParameterSet.Builder builder) {
         return Collections.singletonList(new ItemStack(BlockInit.MOSAIC, state.get(TILES)));
+    }
+    public FluidState getFluidState(BlockState state) {
+        return (Boolean)state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+    }
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(FACING, ctx.getSide());
+    }
+    static {
+        WATERLOGGED = Properties.WATERLOGGED;
+        FACING = Properties.FACING;
     }
 }
