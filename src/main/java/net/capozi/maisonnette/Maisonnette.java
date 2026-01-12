2e2d6ef30b9782452c581b5eaab645e361ab9207
Capozi <capozi.devv@gmail.com>
2026-01-11 21:21:32 -0500

    Update Maisonnette.java

--- src/main/java/net/capozi/maisonnette/Maisonnette.java
+++ src/main/java/net/capozi/maisonnette/Maisonnette.java
@@ -14,7 +14,6 @@ import org.slf4j.LoggerFactory;
 import net.capozi.maisonnette.common.block.BookStackBlock;
 import net.fabricmc.api.ModInitializer;
 
-// NOTE: main
 public class Maisonnette implements ModInitializer {
 	public static final String MOD_ID = "maisonnette";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
