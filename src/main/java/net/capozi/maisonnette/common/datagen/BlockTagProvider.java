3f775f1e9fcba596c84b8921501500a404050637
Capozi <capozi.devv@gmail.com>
2026-01-30 20:50:11 -0500

    sync changes

--- src/main/java/net/capozi/maisonnette/common/datagen/BlockTagProvider.java
+++ src/main/java/net/capozi/maisonnette/common/datagen/BlockTagProvider.java
@@ -5,7 +5,6 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.BlockTags;
-import net.minecraft.registry.tag.ItemTags;
 
 import java.util.concurrent.CompletableFuture;
 
