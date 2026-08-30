8e0f9fbcc249be1290f73753247700920104a425
Capozi <capozi.devv@gmail.com>
2026-08-30 00:02:02 -0400

    carnation sillies

---
+++ src/main/java/net/capozi/maisonnette/mixin/ServerPlayerEntityMixin.java
@@ -0,0 +1,26 @@
+package net.capozi.maisonnette.mixin;
+
+import com.mojang.authlib.GameProfile;
+import net.capozi.maisonnette.foundation.BlockInit;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerPlayerEntity.class)
+public abstract class ServerPlayerEntityMixin extends PlayerEntity {
+    private ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
+        super(world, pos, yaw, gameProfile);
+    }
+
+    @Inject(method = "onDeath", at = @At("HEAD"))
+    private void maisonnette$onDeath(DamageSource damageSource, CallbackInfo ci) {
+        this.getWorld().setBlockState(this.getBlockPos(), BlockInit.CARNATION.getDefaultState());
+        System.out.println("carnatiopn");
+    }
+}
